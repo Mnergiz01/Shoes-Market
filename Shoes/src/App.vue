5d29d8a9b8b44ac26205afda421fbc7439f28b2e
Muzaffer Nergiz <muzaffernergiz0@gmail.com>
2025-03-19 16:01:30 +0300

    Düzenlendi

--- Shoes/src/App.vue
+++ Shoes/src/App.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col min-h-screen">
     
     <main class="container mx-auto mt-10">
-      <Navbar></Navbar>
+      <Navbar class="w-full"></Navbar>
       <router-view></router-view>
     </main>
     <Footer></Footer>
@@ -15,3 +15,10 @@ import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 
 </script>
+
+<style scoped>
+.container {
+  transform: scale(0.9);
+  /* Ekrandaki tüm içeriği %90 oranında küçült */
+}
+</style>
